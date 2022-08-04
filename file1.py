c241390a25575efb4e92879817f47c59a6c41fc5
Gaurav <gsr094@gmail.com>
2022-08-04 13:37:06 +0530

    This is my first code push in git

---
+++ file1.py
@@ -0,0 +1,13 @@
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://Gaurav:mongodb@gaurav.dxwsk.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+
+d ={
+    'name' : 'Gaurav',
+    'lastname' : 'Rajput'
+}
+
+db1 = client['file1']
+coll = db1['test']
+coll.insert_one(d)
